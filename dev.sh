b9a9efea0316f851c63d11addd10b999895e6456
xyuzh <xinyzng@gmail.com>
2025-03-01 00:58:52 -0800

    update to use docker-compose

---
+++ dev.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Stop any existing containers
+docker-compose down
+
+# Rebuild the containers
+docker-compose build
+
+# Start the containers in detached mode
+docker-compose up -d
+
+# Show logs
+docker-compose logs -f 
