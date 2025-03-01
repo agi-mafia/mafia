0a9fca8be90bcfb465a7cebe2924b72177a45e64
Josh Lee <cterdam@google.com>
2025-02-28 23:24:44 -0800

    [model] add model

--- src/game/game_config.py
+++ src/game/game_config.py
@@ -1,5 +1,9 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
+from typing import Dict
+from src.player.role import Role
+from src.model.model import Model
 
 
-class GameConfig(BaseModel, frozen=True):
-    
+class GameConfig(BaseModel):
+    model_config = ConfigDict(strict=True)
+    contestants: Dict[Model, Role]
