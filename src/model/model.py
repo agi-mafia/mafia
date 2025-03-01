d9ce44135817a8e1c053edf47bbc64d74e0be405
Josh Lee <cterdam@google.com>
2025-03-01 00:21:17 -0800

    [model] change model provider signature

--- src/model/model.py
+++ src/model/model.py
@@ -1,2 +1,8 @@
 class Model:
-    pass
+
+    def __init__(self, model_name: str):
+        self.model = model_name
+
+    def inference(self, prompt: str) -> str:
+        # return self.model.predict(prompt)
+        return ""
